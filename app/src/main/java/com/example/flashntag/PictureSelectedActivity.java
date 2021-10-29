8ad90a3b2aaa46969baaa3534db0d949a7b7f048
CrazyCammis <62859438+CrazyCammis@users.noreply.github.com>
2021-10-29 09:23:46 +0200

    Added layout for selcted picture
    Added basic function for selected picture

--- app/src/main/java/com/example/flashntag/PictureSelectedActivity.java
+++ app/src/main/java/com/example/flashntag/PictureSelectedActivity.java
@@ -3,11 +3,17 @@ package com.example.flashntag;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.flashntag.adapter.PictureRecycleAdapter;
+import com.example.flashntag.modeller.Picture;
+
+import java.util.ArrayList;
 
 public class PictureSelectedActivity extends AppCompatActivity {
+    private EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,17 @@ public class PictureSelectedActivity extends AppCompatActivity {
         //TODO: DELETE FROM GALLERY SEE RECYCLBE VIEW LECTURE 55.00;
         //TODO: ADD TAGS
         //TODO: REMOVE TAGS
+
+
+        ArrayList<Picture> dataList = (ArrayList<Picture>) Picture.getData();
+
+        Picture picture = dataList.get(ID);
+
+        Image image = findViewById(R.id.selectedPicuture);
+        text  =findViewById(R.id.date);
+
+
+
 
 
     }
